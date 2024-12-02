c380ce5fb361b723087d5dc31426aeef7dd3b88c
KurniaDuta <dutaka2945@gmail.com>
2024-12-02 13:01:30 +0700

    feat: welcome page and google login auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,6 +2,8 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
+    <a href="{{ route('login.google') }}" class="btn btn-danger">Login with Google</a>
+
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
