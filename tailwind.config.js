ab4bb5085e29edc8c3ec8fc0e882debc33568860
KurniaDuta <dutaka2945@gmail.com>
2024-12-02 13:00:15 +0700

    chore: configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,27 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/js/**/*.js',
     ],
 
     theme: {
+        colors: {
+            'midnight': '#709FA9',
+            white: '#FFFFFF',
+            'oren': '#FBA14B',
+            'gelap': {
+                100: '#D4D4D4',
+                200: '#727272',
+                300: '#2E2E2E',
+                400: '#303030',
+            },
+        },
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Poppins', ...defaultTheme.fontFamily.sans],
             },
+            textColor: ['hover'],
+            backgroundColor: ['hover'],
         },
     },
 
